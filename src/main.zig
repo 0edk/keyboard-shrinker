167941823f2ca407243bf2e94bcb3aabccc0070c
dkl9 <contact@dkl9.net>
2025-07-25 06:42:37 +0000

    Use arrays in `Trie` instead of `ArrayList`

--- src/main.zig
+++ src/main.zig
@@ -5,114 +5,80 @@ const String = std.ArrayList(u8);
 pub fn Trie(comptime K: type, comptime V: type) type {
     return struct {
         const Self = @This();
+        const N = 1 << @typeInfo(K).int.bits;
         leaf: V = 0,
-        children: std.AutoHashMap(K, *Self),
+        children: [N]?*Self = [_]?*Self{null} ** N,
         allocator: Allocator,
 
         pub fn init(allocator: Allocator) Self {
-            return Self{
-                .children = std.AutoHashMap(K, *Self).init(allocator),
-                .allocator = allocator,
-            };
+            return Self{ .allocator = allocator, };
         }
 
         pub fn deinit(self: *Self) void {
-            var vit = self.children.valueIterator();
-            while (vit.next()) |child| {
-                child.*.deinit();
-                self.allocator.destroy(child.*);
+            for (self.children) |maybe_child| {
+                if (maybe_child) |child| {
+                    child.deinit();
+                    self.allocator.destroy(child);
+                }
             }
-            self.children.deinit();
         }
 
         pub fn get(self: *Self, key: []const K) Allocator.Error!*Self {
             if (key.len == 0) {
                 return self;
             } else {
-                if (self.children.get(key[0])) |child| {
+                if (self.children[key[0]]) |child| {
                     return child.get(key[1..]);
                 } else {
                     const child = try self.allocator.create(Self);
                     child.* = Self.init(self.allocator);
-                    try self.children.put(key[0], child);
-                    return self.children.get(key[0]).?.get(key[1..]);
+                    self.children[key[0]] = child;
+                    return child.get(key[1..]);
                 }
             }
         }
 
         pub fn show(self: *const Self, writer: anytype, depth: usize) !void {
-            var it = self.children.iterator();
-            while (it.next()) |child| {
-                try writer.writeByteNTimes(' ', 2 * depth);
-                try writer.print("{c}{s}\n", .{ child.key_ptr.*, if (child.value_ptr.*.leaf != 0) "$" else "" });
-                try child.value_ptr.*.show(writer, depth + 1);
+            for (0..self.children.len) |i| {
+                if (self.children[i]) |child| {
+                    for (0..depth) |_| {
+                        std.debug.print("  ", .{});
+                    }
+                    std.debug.print("{d}{s}\n", .{ i, if (child.leaf != 0) "$" else "" });
+                    try child.show(writer, depth + 1);
+                }
             }
         }
     };
 }
 
-const WordTrie = Trie(u8, u32);
+const Letter = u5;
 
-const MatchIterator = struct {
-    root: WordTrie,
-    branch_iters: std.ArrayList(std.AutoHashMap(u8, *WordTrie).Iterator),
-    subset: []const u8,
-    query: []u8,
-    index: usize = 0,
-    prefix: String,
-
-    // very broken
-    fn next(self: *MatchIterator) Allocator.Error!?[]const u8 {
-        if (self.branch_iters.pop()) |it_old| {
-            var it = it_old;
-            if (it.next()) |n| {
-                std.debug.print("n is of type {any}\n", .{@TypeOf(n)});
-                const letter = n.key_ptr.*;
-                const child = n.value_ptr.*;
-                std.debug.print("found child '{u}': {any}\n", .{ letter, child });
-                if (self.query.len > self.index and letter == self.query[self.index]) {
-                    std.debug.print("which matches query at {d}\n", .{self.index});
-                    self.index += 1;
-                    try self.prefix.append(letter);
-                    try self.branch_iters.append(it);
-                    try self.branch_iters.append(child.children.iterator());
-                    return if (child.leaf > 0) self.prefix.items else self.next();
-                } else {
-                    for (self.subset) |c| {
-                        if (letter == c) {
-                            std.debug.print("which is in subset\n", .{});
-                            try self.branch_iters.append(it);
-                            return self.next();
-                        }
-                    } else {
-                        std.debug.print("which is outside subset\n", .{});
-                        try self.prefix.append(letter);
-                        try self.branch_iters.append(it);
-                        try self.branch_iters.append(child.children.iterator());
-                        return if (child.leaf > 0) self.prefix.items else self.next();
-                    }
-                }
-            }
-            std.debug.print("out of children\n", .{});
-            _ = self.prefix.pop();
-            return self.next();
+fn charsToLetters(allocator: Allocator, str: []const u8) Allocator.Error!std.ArrayList(Letter) {
+    var projected = std.ArrayList(Letter).init(allocator);
+    for (str) |c| {
+        if (std.ascii.isAlphabetic(c)) {
+            try projected.append(@intCast(std.ascii.toLower(c) - 'a'));
         }
-        std.debug.print("done with root\n", .{});
-        return null;
     }
-};
+    return projected;
+}
 
-pub fn expandInput(subset: []const u8, trie: WordTrie, query: []u8) Allocator.Error!MatchIterator {
-    const alloc = trie.allocator;
-    var branches = std.ArrayList(std.AutoHashMap(u8, *WordTrie).Iterator).init(alloc);
-    try branches.append(trie.children.iterator());
-    return MatchIterator{
-        .root = trie,
-        .branch_iters = branches,
-        .subset = subset,
-        .query = query,
-        .prefix = String.init(alloc),
-    };
+fn lettersToChars(allocator: Allocator, letters: []const Letter) Allocator.Error!std.ArrayList(Letter) {
+    var s = String.initCapacity(allocator, letters.len);
+    for (letters) |l| {
+        s.append(l + 'a');
+    }
+    return s;
+}
+
+const WordTrie = Trie(Letter, u32);
+
+// TODO
+pub fn expandInput(subset: []const Letter, trie: WordTrie, query: []Letter) Allocator.Error!void {
+    _ = subset;
+    _ = trie;
+    _ = query;
 }
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -131,7 +97,7 @@ pub fn main() !void {
     var buf: [128]u8 = undefined;
     while (try wlr.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         i += 1;
-        (try dict_trie.get(line)).leaf = 1000 / i;
+        (try dict_trie.get((try charsToLetters(main_alloc, line)).items)).leaf = 1000 / i;
     }
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
@@ -139,15 +105,12 @@ pub fn main() !void {
     const stdin_file = std.io.getStdIn().reader();
     var br = std.io.bufferedReader(stdin_file);
     const stdin = br.reader();
-    try dict_trie.show(stdout, 0);
-    try bw.flush();
     while (try stdin.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         var it = try expandInput("etao", dict_trie, line);
-        _ = &it;
-        //while (try it.next()) |match| {
-        //    try stdout.print("{s}\n", .{match});
-        //    try bw.flush();
-        //}
+        while (try it.next()) |match| {
+            try stdout.print("{s}\n", .{match});
+            try bw.flush();
+        }
     }
 }
 
@@ -156,10 +119,15 @@ test "trie on string" {
     defer trie.deinit();
     const words = [_][]const u8{ "a", "to", "tea", "ted", "ten", "i", "in", "inn" };
     for (words) |word| {
-        const branch = try trie.get(word);
+        const letters = try charsToLetters(std.testing.allocator, word);
+        const branch = try trie.get(letters.items);
+        letters.deinit();
         branch.leaf += 1;
     }
     for (words) |word| {
-        try std.testing.expectEqual(1, (try trie.get(word)).leaf);
+        const letters = try charsToLetters(std.testing.allocator, word);
+        try std.testing.expectEqual(1, (try trie.get(letters.items)).leaf);
+        letters.deinit();
     }
+    try trie.show(void, 0);
 }
